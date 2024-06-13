e98025bd0ce48f2c0b91e985c1d940e9d43d32dc
ehab <ehaborabi94@gmail.com>
2024-06-13 19:08:56 +0300

    webhook

--- polybot/bot.py
+++ polybot/bot.py
@@ -24,7 +24,7 @@ class Bot:
         # set the webhook URL
         self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{token}/', timeout=60)
 
-        with open("YOURPUBLIC.pem", 'rb') as cert:
+        with open("YOURPUBLIC.pem", 'r') as cert:
             self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{token}/', certificate=cert, timeout=60)
 
         logger.info(f'Telegram Bot information\n\n{self.telegram_bot_client.get_me()}')
